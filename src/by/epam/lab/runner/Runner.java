4e4824fc5556ee39e146dca9071f81524d7e1828
Shelzi <nemant38@gmail.com>
2022-01-09 19:04:20 +0300

    after guide

--- src/by/epam/lab/runner/Runner.java
+++ src/by/epam/lab/runner/Runner.java
@@ -8,53 +8,49 @@ import by.epam.lab.util.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 public class Runner {
     public static void main(String[] args) {
-        List<Trial> trials = new ArrayList<Trial>() {
-            {
-                add(new Trial("Sakovich", 45, 93));
-                add(new Trial("Kerkashin", 100, 100));
-                add(new Trial("Zhylinsky", 51, 35));
-                add(new LiteTrial("Barkou", 29, 95));
-                add(new LiteTrial("Lisoniana", 35, 68));
-                add(new StrongTrial("Astapenka", 30, 57));
-                add(new StrongTrial("Vapenka", 90, 80));
-                add(new ExtraTrial("Megabrain", 90, 90, 60));
-                add(new ExtraTrial("Smartguy", 89, 90, 80));
-            }
-        };
+        List<Trial> trials = new ArrayList<>(Arrays.asList(
+                new Trial("Sakovich", 45, 93),
+                new Trial("Kerkashin", 100, 100),
+                new Trial("Zhylinsky", 51, 35),
+                new LiteTrial("Barkou", 29, 95),
+                new LiteTrial("Lisoniana", 35, 68),
+                new StrongTrial("Astapenka", 30, 57),
+                new StrongTrial("Vapenka", 90, 80),
+                new ExtraTrial("Megabrain", 90, 90, 60),
+                new ExtraTrial("Smartguy", 89, 90, 80))); //1
 
-        Utils.printTrials(trials);
+        Utils.printTrials(trials); //2
         System.out.println();
 
-        trials.stream().filter(Trial::isPassed).forEach(System.out::println);
+        trials.stream().filter(Trial::isPassed).forEach(System.out::println); //3
         System.out.println();
 
-        trials = trials.stream().sorted(Trial::compareTo).collect(Collectors.toList());
+        Function<Trial, Integer> sumMarks = x -> x.getMark1() + x.getMark2();
+        trials.sort(Comparator.comparingInt(sumMarks::apply)); //4
         System.out.println();
 
-        Utils.printTrials(trials);
-        System.out.println();
-
-        trials.stream().forEach(s -> System.out.println(s.getMark1() + s.getMark2()));
+        trials.stream().map(sumMarks).forEach(System.out::println); //5
         System.out.println();
 
         List<Trial> nonPassedTrialsClone = trials.stream()
                 .filter((p) -> !p.isPassed())
                 .map(Trial::clone)
                 .peek(Trial::refreshMarks)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()); //6
         Utils.printTrials(nonPassedTrialsClone);
         System.out.println();
 
         int[] arr = trials.stream()
                 .mapToInt((ToIntFunction<? super Trial>) t -> (t.getMark1() + t.getMark2()))
-                .toArray();
-        System.out.println();
+                .toArray(); //7
 
         Arrays.stream(arr).forEach(System.out::println);
         System.out.println();

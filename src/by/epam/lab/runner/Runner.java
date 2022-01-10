09a2d1d90c35c8ed3e4e7aed2c089b584103651c
Shelzi <nemant38@gmail.com>
2022-01-10 19:50:43 +0300

    fix after review, +Runner.class

--- src/by/epam/lab/runner/Runner.java
+++ src/by/epam/lab/runner/Runner.java
@@ -4,7 +4,6 @@ import by.epam.lab.entity.ExtraTrial;
 import by.epam.lab.entity.LiteTrial;
 import by.epam.lab.entity.StrongTrial;
 import by.epam.lab.entity.Trial;
-import by.epam.lab.util.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,32 +26,40 @@ public class Runner {
                 new ExtraTrial("Megabrain", 90, 90, 60),
                 new ExtraTrial("Smartguy", 89, 90, 80))); //1
 
-        Utils.printTrials(trials); //2
+        trials.forEach(System.out::println); //2
         System.out.println();
 
-        trials.stream().filter(Trial::isPassed).forEach(System.out::println); //3
-        System.out.println();
+        System.out.println(trials.stream()
+                .filter(Trial::isPassed)
+                .count()); //3
 
         Function<Trial, Integer> sumMarks = x -> x.getMark1() + x.getMark2();
-        trials.sort(Comparator.comparingInt(sumMarks::apply)); //4
+        ToIntFunction<? super Trial> sumMarksToInt = x -> x.getMark1() + x.getMark2();
+
+        trials.sort(Comparator.comparingInt(sumMarksToInt)); //4
         System.out.println();
 
-        trials.stream().map(sumMarks).forEach(System.out::println); //5
+        trials.stream()
+                .map(sumMarks)
+                .forEach(System.out::println); //5
         System.out.println();
 
         List<Trial> nonPassedTrialsClone = trials.stream()
                 .filter((p) -> !p.isPassed())
-                .map(Trial::clone)
+                .map(Trial::getCopy)
                 .peek(Trial::refreshMarks)
+                .peek(System.out::println)
                 .collect(Collectors.toList()); //6
-        Utils.printTrials(nonPassedTrialsClone);
+        System.out.println("\nAll trials is not pass: " + nonPassedTrialsClone.stream().noneMatch(Trial::isPassed));
         System.out.println();
 
         int[] arr = trials.stream()
-                .mapToInt((ToIntFunction<? super Trial>) t -> (t.getMark1() + t.getMark2()))
+                .mapToInt(sumMarksToInt)
                 .toArray(); //7
 
-        Arrays.stream(arr).forEach(System.out::println);
+        System.out.println(Arrays.stream(arr)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", ")));
         System.out.println();
     }
 }
